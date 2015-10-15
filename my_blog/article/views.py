8fc488ddb1e826c88fcad90f0d8ad385c96fa45b
Jian Dai <daijian1@qq.com>
2015-10-16 02:47:55 +0800

    1. install pep8; 2. modify views.py follow pep8 instruction.

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -3,13 +3,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
+from django.http import Http404
 
-# Create your views here.
+from datetime import datetime
+
+
 def home(request):
-    post_list = Article.objects.all() #获取全部的Article对象
+    # 获取全部的Article对象
+    post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
 
-from django.http import Http404
+
 def detail(request, id):
     try:
         post = Article.objects.get(id=str(id))
@@ -17,6 +21,6 @@ def detail(request, id):
         raise Http404
     return render(request, 'post.html', {'post': post})
 
-from datetime import datetime
+
 def test(request):
     return render(request, 'test.html', {'current_time': datetime.now()})
