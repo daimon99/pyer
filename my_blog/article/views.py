d768db1c88da7ce4d88fc4b63a9ad7ed916176ad
Jian Dai <daijian1@qq.com>
2015-10-16 00:41:24 +0800

    A blog system. Now all pages are writen ok.

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,10 +1,14 @@
+# -*- coding:utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello World, Django")
+#    return HttpResponse("Hello World, Django")
+    post_list = Article.objects.all() #获取全部的Article对象
+    return render(request, 'home.html', {'post_list': post_list})
 
 def detail(request, my_args):
     post = Article.objects.all()[int(my_args)]
