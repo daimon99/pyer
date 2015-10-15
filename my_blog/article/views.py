e65cafcd4800d3c6e929b69c914f79185b8981a1
Jian Dai <daijian1@qq.com>
2015-10-15 17:55:17 +0800

    add db support. read db data and return it to the web. easy.

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import Article
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello World, Django")
 
 def detail(request, my_args):
-    return HttpResponse('Hello %s' % my_args)
+    post = Article.objects.all()[int(my_args)]
+    str = ('title = %s, category = %s, date_time = %s, content = %s' % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
