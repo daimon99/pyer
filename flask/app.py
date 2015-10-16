8c8a7eea9a9ee51b98326b4255460f68548aeb41
Jian Dai <daijian1@qq.com>
2015-10-16 18:23:23 +0800

    Use Data-Center method to programe. Map method to map the config vars, and then connect them using str function. then print

--- flask/app.py
+++ flask/app.py
@@ -8,5 +8,7 @@ app=Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 # 现在通过app.config["VAR_NAME"]，我们可以访问到对应的变量
-allconfig = '\n'.join(['%s: %s' % (k, v) for k, v in app.config.items()])
+# allconfig = '\n'.join(['%s: %s' % (k, v) for k, v in app.config.items()])
+f = lambda a: '%s=%s' % (a[0], a[1])
+allconfig = '\n'.join(map(f, app.config.items()))
 print allconfig
