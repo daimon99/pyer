9bdbec0869b392e7ed97ea4f98652795bf8e4675
Jian Dai <daijian1@qq.com>
2015-10-16 18:16:37 +0800

    Practise settings in flask--config Vs. instance/config. And print all config vars using "for"

---
+++ flask/app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from flask import Flask
+
+
+app=Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+# 现在通过app.config["VAR_NAME"]，我们可以访问到对应的变量
+allconfig = '\n'.join(['%s: %s' % (k, v) for k, v in app.config.items()])
+print allconfig
---
+++ flask/config.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+DEBUG = False # 启动flask的debug模式
+BCRYPT_LEVEL = 13 # 配置Flask-Bcrypt扩展
+MAIL_FROM_EMAIL = 'daijian1@qq.com' # 设置邮件来源
