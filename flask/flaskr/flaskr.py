6c0b1e449585d727a941191449799c91d808b7bc
Jian Dai <daijian1@qq.com>
2015-10-19 20:14:45 +0800

    1. Reorg code, add comment; 2. @teardown_request, @before_request, @after_request, teardown has args--exception or None. Use this decorators to gracefully using db connection; 3. Using getattr to avoid a proxy throwing a method not exist exception; 4. the row_factory property, I will study it in the following lectures but not now.

--- flask/flaskr/flaskr.py
+++ flask/flaskr/flaskr.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python
-#-*-coding:utf8-*-
+# -*- coding:utf8 -*-
+"""
+    Flaskr
+    ~~~~~~
+
+    A microblog example application written as Flask tutorial with
+    Flask and sqlite3
+
+    :copyright: (c) 2015 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
+
 # all the imports
 import sqlite3
+import os
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
 from contextlib import closing
 
 
-# configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
-
 # create our little application :)
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE = os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG = True,
+    SECRET_KEY = 'development key',
+    USERNAME = 'admin',
+    PASSWORD = 'default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
 def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
 
 
 def init_db():
@@ -29,6 +45,19 @@ def init_db():
         with app.open_resource('schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+    g.db.close()
 
 
 if __name__ == '__main__':
