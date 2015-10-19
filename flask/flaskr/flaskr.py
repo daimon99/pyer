466060410b7a6a18caee86ea769ab74b581fcde6
Jian Dai <daijian1@qq.com>
2015-10-19 18:33:44 +0800

    1. with: __enter__(), __exit__() is executed; 2. with closing: close() is executed after code complete. 3. db.cursor().executescript() to execute a sql script. 4. app.open_resouce() to open a file, and it has the __enter__ and __exit__ method

--- flask/flaskr/flaskr.py
+++ flask/flaskr/flaskr.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python
+#-*-coding:utf8-*-
 # all the imports
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash
+from contextlib import closing
 
 
 # configuration
@@ -19,6 +22,14 @@ app.config.from_object(__name__)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
